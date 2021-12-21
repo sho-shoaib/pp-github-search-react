4f34efd5696a2be1c52f14dcbce9db36772e5d67
Shoaib Shaikh <shaikhsho292@gmail.com>
2021-12-21 21:59:22 +0530

    follower component half completed

---
+++ src/context/context.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect, useContext } from 'react';
+import mockUser from './mockData.js/mockUser';
+import mockRepos from './mockData.js/mockRepos';
+import mockFollowers from './mockData.js/mockFollowers';
+import axios from 'axios';
+
+const rootUrl = 'https://api.github.com';
+
+const GithubContext = React.createContext()
+
+const GithubProvider = ({children}) => {
+  const [user,setUsers] = useState(mockUser)
+  const [repos,setRepos] = useState(mockRepos)
+  const [followers,setFollowers] = useState(mockFollowers)
+
+  return(
+    <GithubContext.Provider value={{user,repos,followers}}>
+      {children}
+    </GithubContext.Provider>
+  )
+}
+
+export const useGlobalContext = () => {
+  return useContext(GithubContext)
+}
+
+export {GithubProvider, GithubContext}
